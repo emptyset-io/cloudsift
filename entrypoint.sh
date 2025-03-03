c69385cee976f6e4d5b3f7c09013011892eb685e
Chris Molle <chris@emptyset.io>
2025-03-03 12:42:00 -0700

    remove entrypoint

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Ensure group-based access to AWS credentials
-AWS_GID=$(stat -c "%g" /home/cloudsift/.aws)   # Get GID of the mounted directory
-addgroup -g $AWS_GID aws 2>/dev/null || true   # Create the group (if doesn't exist)
-adduser cloudsift aws 2>/dev/null || true      # Add cloudsift to the group (if not already)
-
-# Now run the main command
-exec "$@"
